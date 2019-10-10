45ab62fdee96b9854e741c13bd1dd8eccd9fecf5
yasmine <yasmine.ezequiel@gmail.com>
2019-10-10 17:50:14 +0200

    Adds feature files and webpack.

---
+++ features/step_definitions/basic-steps.js
@@ -0,0 +1,11 @@
+const { Given, Then } = require("cucumber");
+
+Given("I visit the site", function() {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("I should see {string}", function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
